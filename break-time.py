59064fca5b114fa07eb77f4a8414b0b7c3d7efa3
Carlos Abdalla <abdalla@msdev.com.br>
2017-02-27 21:57:39 -0500

    new link

--- break-time.py
+++ break-time.py
@@ -7,5 +7,5 @@ count = 0
 print('Break time is running - '+ time.ctime())
 while (count < REPTIME):
     time.sleep(10);
-    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://youtu.be/44LdLqgOpjo")
     count = count + 1
